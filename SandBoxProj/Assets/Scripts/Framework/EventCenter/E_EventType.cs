b923342253340c4c0c1bac69179c369103a06f7c
LaoDa <571119800@qq.com>
2026-08-24 21:44:32 +0800

    [新]事件中心

---
+++ SandBoxProj/Assets/Scripts/Framework/EventCenter/E_EventType.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 事件类型 枚举
+/// </summary>
+public enum E_EventType 
+{
+    E_Default,
+    E_Test,
+    E_BuildButtonClick,
+
+}
