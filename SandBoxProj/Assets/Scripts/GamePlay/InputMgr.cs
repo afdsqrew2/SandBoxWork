d7e571ee3aec030b0ce6598c31adf39246f73439
LaoDa <571119800@qq.com>
2026-08-24 21:25:19 +0800

    [新]Gamplay相关逻辑

---
+++ SandBoxProj/Assets/Scripts/GamePlay/InputMgr.cs
@@ -0,0 +1,60 @@
+using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+
+public class InputMgr : MonoBehaviour
+{
+    [SerializeField]
+    private Camera sceneCamera;
+
+    private Vector3 lastPosition;
+
+    [SerializeField]
+    private LayerMask placementLayermask;
+
+    public event Action OnClicked, OnExit;
+
+    private void Update()
+    {
+        //yyf 老·输入系统报错
+        /*
+        if(Input.GetMouseButtonDown(0))
+            OnClicked?.Invoke();
+        if(Input.GetKeyDown(KeyCode.Escape))
+            OnExit?.Invoke();
+            */
+        if(Mouse.current!=null && Mouse.current.leftButton.isPressed)
+            OnClicked?.Invoke();
+        
+        
+        if(Touchscreen.current!= null && Touchscreen.current.touches.Count>0 && Touchscreen.current.touches[0].press.wasPressedThisFrame)
+            OnClicked?.Invoke();
+    }
+
+    public bool IsPointerOverUI()
+        => EventSystem.current.IsPointerOverGameObject();
+
+    public Vector3 GetSelectedMapPosition()
+    {
+        Vector3 mousePos = Vector3.zero;
+        if (Mouse.current != null)
+        {
+             mousePos = Mouse.current.position.ReadValue();
+        }
+        
+        if (Touchscreen.current!= null && Touchscreen.current.touches.Count>0)
+        {
+            mousePos = Touchscreen.current.touches[0].position.ReadValue();
+        }
+
+        mousePos.z = sceneCamera.nearClipPlane;
+        Ray ray = sceneCamera.ScreenPointToRay(mousePos);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, 100, placementLayermask))
+        {
+            lastPosition = hit.point;
+        }
+        return lastPosition;
+    }
+}
---
+++ SandBoxProj/Assets/Scripts/GamePlay/PlaceMgr.cs
@@ -0,0 +1,111 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlacementSystem : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject mouseIndicator,cellingIndicator;
+    [SerializeField]
+    private InputMgr inputManager;
+    [SerializeField]
+    private Grid grid;
+
+    [SerializeField]
+    private ObjectsDatabaseSO database;
+
+    private int seletedObjectIndex = -1;
+
+    [SerializeField]
+    private GameObject gridVisualization;
+
+    private GridData floorData, furniData;
+    private Renderer previewRenderer;
+
+    private List<GameObject> placedGameObjects = new List<GameObject>();
+    private void Start()
+    {
+        StopPlacement();
+        floorData = new GridData();
+        furniData = new GridData();
+        previewRenderer = cellingIndicator.GetComponentInChildren<Renderer>();
+    }
+
+    public void StartPlacement(int ID)
+    {
+        StopPlacement();
+        seletedObjectIndex = database.objectsData.FindIndex(data => data.ID == ID);
+        if (seletedObjectIndex < 0)
+        {
+            Debug.LogError($"no id found {ID}");
+        }
+        gridVisualization.gameObject.SetActive(true);
+        cellingIndicator.SetActive(true);
+        inputManager.OnClicked += PlaceStructure;
+        inputManager.OnExit += StopPlacement;
+        
+
+    }
+
+    private void PlaceStructure()
+    {
+        if(inputManager.IsPointerOverUI())
+        {
+            return;
+        }
+        Vector3 mousePosition = inputManager.GetSelectedMapPosition();
+        Vector3Int gridPosition = grid.WorldToCell(mousePosition);
+
+        bool placeValid = CheckPlaceValid(gridPosition, seletedObjectIndex);
+        if (!placeValid)
+        {
+            return;
+        }
+        
+        GameObject newGO = Instantiate(database.objectsData[seletedObjectIndex].Prefab);
+        newGO.transform.position = grid.CellToWorld(gridPosition);
+
+        //GridData seletedData = database.objectsData[seletedObjectIndex].ID == 0 ? floorData : furniData;
+        GridData seletedData = furniData;
+        seletedData.AddObjectAt(gridPosition,database.objectsData[seletedObjectIndex].Size,
+            database.objectsData[seletedObjectIndex].ID,placedGameObjects.Count-1);
+        
+        placedGameObjects.Add(newGO);
+
+    }
+
+    private bool CheckPlaceValid(Vector3Int gridPosition, int selectedIndex)
+    {
+        //GridData selectedData = seletedObjectIndex == 0 ? floorData : furniData;
+        GridData selectedData = furniData;
+        return selectedData.CanPlaceObejctAt(gridPosition, database.objectsData[selectedIndex].Size);
+    }
+
+    private void StopPlacement()
+    {
+        //throw new NotImplementedException();
+        seletedObjectIndex = -1;
+        gridVisualization.gameObject.SetActive(false);
+        cellingIndicator.SetActive(false);
+        inputManager.OnClicked -= PlaceStructure;
+        inputManager.OnExit -= StopPlacement;
+    }
+
+
+    private void Update()
+    {
+        if (seletedObjectIndex < 0)
+        {
+            return;
+        }
+
+        Vector3 mousePosition = inputManager.GetSelectedMapPosition();
+        Vector3Int gridPosition = grid.WorldToCell(mousePosition);
+        
+        bool placeValid = CheckPlaceValid(gridPosition, seletedObjectIndex);
+
+        previewRenderer.material.color = placeValid ? Color.white : Color.red;
+        
+        mouseIndicator.transform.position = mousePosition;
+        cellingIndicator.transform.position = grid.CellToWorld(gridPosition);
+    }
+}
